82e7980541ef76390c0afad2ed452de7ef567437
mugiwarafx <sangalesbenes@uoc.edu>
2022-07-01 14:41:46 +0200

    adds part 13 exercise 6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,13 +17,26 @@ router.post('/', async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-  return res.json(blog)
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+  }
+  res.status(204).end()
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
 })
 
 module.exports = router
